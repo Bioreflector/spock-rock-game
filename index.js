6fc35058adfff98c62a034b1669e6346cb5db230
Alexandr <Bioreflector@gmail.com>
2022-11-04 12:39:47 +0200

    created function game

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let computerScore = 0;
 const palyerButtons = document.querySelectorAll('.player-butoon')
 const youChoseOut = document.querySelector('.you-choise')
 const comuterChoiseOut = document.querySelector('.computer-choise')
+const playerScoreOut = document.querySelector('.player-score')
+const computerScoreOut = document.querySelector('.computer-score')
+const massage = document.querySelector('.massage')
+console.log(massage)
 
 const valueGameArr =['rock' , 'paper' , 'scissors']
 
@@ -12,15 +16,46 @@ palyerButtons.forEach(button => button.addEventListener('click' , startGame))
 
 function startGame(){
     const playerValue = this.id
+    youChoseOut.innerText = this.id
     const computerValue = computerChose()
-    console.log(computerValue)
-    console.log(playerValue)
+    game(playerValue , computerValue)
+}
+
+function game(playerValue , computerValue){
+    if(playerValue == 'rock' && computerValue == 'scissors'){
+        youScore++
+        playerScoreOut.innerText = youScore
+        massage.innerText = 'You Win'
+    }
+    else if(playerValue == 'paper' && computerValue == 'rock'){
+        youScore++
+        playerScoreOut.innerText = youScore
+        massage.innerText = 'You Win'
+    }
+    else if(playerValue == 'scissors' && computerValue == 'paper'){
+        youScore++
+        playerScoreOut.innerText = youScore
+        massage.innerText = 'You Win'
+    }
+    else if(playerValue == 'rock' && computerValue == 'rock'){
+        massage.innerText = 'Oo'
+    }
+    else if(playerValue == 'paper' && computerValue == 'paper'){
+        massage.innerText = 'Oo'
+    }
+    else if(playerValue == 'scissors' && computerValue == 'scissors'){
+        massage.innerText = 'Oo'
+    }
+    else{
+        computerScore++
+        computerScoreOut.innerText = computerScore
+        massage.innerText = 'You Lose'
+    }
 }
 
 function random(max){
     return Math.floor(Math.random() * max)
 }
-
 function computerChose(){
     const computerValue = valueGameArr[random(valueGameArr.length)]
     comuterChoiseOut.innerText = computerValue
